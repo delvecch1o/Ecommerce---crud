9364bfd993aaee3d21f39c757d8cd9001bc73320
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-08-23 18:50:53 -0300

    front

---
+++ front/src/layouts/admin/MasterLayout.js
@@ -0,0 +1,15 @@
+import React from 'reacrt';
+
+import Navbar from './Navbar';
+
+const MasterLayout = () => {
+
+    return(
+        <div className="sb-nav-fixed">
+            <Navbar />
+
+        </div>
+    )
+}
+
+export default MasterLayout
