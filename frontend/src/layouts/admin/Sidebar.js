9a9417a1069ed7fa047edcf9ab053273aa8f703d
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-09-02 19:46:06 -0300

    Add Category

--- frontend/src/layouts/admin/Sidebar.js
+++ frontend/src/layouts/admin/Sidebar.js
@@ -16,9 +16,13 @@ const Sidebar = () => {
                     <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
                     Profile
                 </Link>
-                <Link className="nav-link" to="/admin/category">
+                <Link className="nav-link" to="/admin/add-category">
                     <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
-                    Category
+                   Add Category
+                </Link>
+                <Link className="nav-link" to="/admin/view-category">
+                    <div className="sb-nav-link-icon"><i className="fas fa-tachometer-alt"></i></div>
+                    View Category
                 </Link>
                 <div className="sb-sidenav-menu-heading">Interface</div>
                 <Link className="nav-link collapsed" to="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
