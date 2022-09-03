74b67292086b0da4c62abedd5359197033cb12a6
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-09-03 08:56:09 -0300

    view

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -4,6 +4,7 @@ import Category from "../components/admin/category/Category";
 import ViewCategory from "../components/admin/category/ViewCategory";
 import EditCategory from "../components/admin/category/EditCategory";
 import AddProduct from "../components/admin/product/AddProduct";
+import ViewProduct from "../components/admin/product/ViewProduct";
 
 
 
@@ -14,6 +15,7 @@ const routes = [
     { path: '/admin/view-category', exact: true, name: 'ViewCategory', component: ViewCategory },
     { path: '/admin/edit-category/:id', exact: true, name: 'EditCategory', component: EditCategory },
     { path: '/admin/add-product', exact: true, name: 'AddProduct', component: AddProduct },
+    { path: '/admin/view-product', exact: true, name: 'ViewProduct', component: ViewProduct },
     { path: '/admin/profile', exact: true, name: 'Profile', component: Profile },
 
 
