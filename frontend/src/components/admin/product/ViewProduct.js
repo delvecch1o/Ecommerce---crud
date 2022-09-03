3771e71f3636dcf3f3a5e4cc56db915af5b07ae5
delvecch1o <rodrigopereirapinheiro@gmail.com>
2022-09-03 11:48:29 -0300

    edit

--- frontend/src/components/admin/product/ViewProduct.js
+++ frontend/src/components/admin/product/ViewProduct.js
@@ -1,18 +1,18 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-function ViewProduct(){
-    
+function ViewProduct() {
+
     const [loading, setLoading] = useState(true);
     const [viewProduct, setProduct] = useState([]);
-    
+
     useEffect(() => {
 
         document.title = " View Product";
-        
+
         axios.get(`/api/view-product`).then(res => {
-            if(res.data.status === 200){
+            if (res.data.status === 200) {
 
                 setProduct(res.data.products);
                 setLoading(false);
@@ -22,25 +22,27 @@ function ViewProduct(){
         });
 
     }, []);
-    
+
     var display_Productdata = "";
-    
-    if(loading){
+
+    if (loading) {
 
         return <h4>View Products.. </h4>
 
-    } else{
+    } else {
 
-        display_Productdata = viewProduct.map( (item) => {
-            
-            return(
+        display_Productdata = viewProduct.map((item) => {
+
+            return (
                 <tr key={item.id}>
                     <td>{item.id}</td>
                     <td>{item.category.name}</td>
                     <td>{item.name}</td>
                     <td>{item.selling_price}</td>
                     <td><img src={`http://localhost:8000/${item.image}`} width="50px" alt={item.name}></img></td>
-                    <td><Link to="edit-product" className='btn btn-success btn-sm'>Edit</Link></td>
+                    <td>
+                        <Link to={`edit-product/${item.id}`} className='btn btn-success btn-sm'>Edit</Link>
+                    </td>
                     <td><button type="button" className='btn btn-danger btn-sm'>Delete</button></td>
                 </tr>
             )
@@ -49,11 +51,11 @@ function ViewProduct(){
     }
 
 
-    return(
+    return (
         <div className='card px-4 mt-3'>
             <div className='card-header'>
                 <h4>View Product
-                    <Link to="/admin/view-product" className="btn btn-primary btn-sm float-end">Add Product</Link> 
+                    <Link to="/admin/view-product" className="btn btn-primary btn-sm float-end">Add Product</Link>
                 </h4>
             </div>
             <div className='card-body'>
